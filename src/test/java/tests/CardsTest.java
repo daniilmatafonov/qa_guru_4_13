d0b14a03e6ad49858626d264aa13edf43b20c5a8
Daniil Matafonov <danielmt.dm@gmail.com>
2021-04-04 18:38:48 +0300

    add cards test

---
+++ src/test/java/tests/CardsTest.java
@@ -0,0 +1,25 @@
+package tests;
+
+import io.qameta.allure.Feature;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import pages.CardsPage;
+
+import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.sleep;
+
+@Tag("web")
+@Tag("cards")
+public class CardsTest extends BaseTest{
+
+    private final CardsPage cardsPage = new CardsPage();
+
+    @Tag("credit_cards")
+    @Test
+    public void showCreditCards() {
+        open("/");
+        cardsPage.closeInvitationPopUp();
+        cardsPage.openCardsSection();
+        cardsPage.clickCheckBoxDebitCards();
+    }
+}
